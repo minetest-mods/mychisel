b860ec6fd8d8979636f3f53d920e544e90f8f25f
DonBatman <serfdon@gmail.com>
2015-09-08 11:20:09 -0700

    Small correction

--- init.lua
+++ init.lua
@@ -1,9 +1,5 @@
-mycorners = {}
 dofile(minetest.get_modpath("mychisel").."/chisel.lua")
---dofile(minetest.get_modpath("mychisel").."/horizontal_nodes.lua")
---dofile(minetest.get_modpath("mychisel").."/point_nodes.lua")
 dofile(minetest.get_modpath("mychisel").."/nodes.lua")
---dofile(minetest.get_modpath("mychisel").."/cross_nodes.lua")
 
 
 
